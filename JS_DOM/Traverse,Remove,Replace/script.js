5306c8e5e3a3043195953e41dd2b6e0bb3138476
github <pragya.sharma2_cs19@gla.ac.in>
2021-04-27 18:14:25 +0530

    explaination of transversal

--- JS_DOM/Traverse,Remove,Replace/script.js
+++ JS_DOM/Traverse,Remove,Replace/script.js
@@ -1,11 +1,26 @@
-temp2.parentElement.parentElement.previousElementSibling.children[0].children[3]
-temp2.previousElementSibling
-temp2.nextElementSibling
-temp2.parentElement.remove(mi)
+temp2=document.getElementById('two')
+
+temp2.parentElement // parent element of the item having id = two is ol i.e ordered list
+
+temp2.parentElement.parentElement.previousElementSibling.children[0].children[2]
+//      ol             li(bee)         li(ay)               ol         li(gamma)
+
+temp2.previousElementSibling //gives the complete next li item
+
+temp2.nextElementSibling //three
+
+temp2.parentElement.children[1].remove() //remove the element two
+
 temp2.parentElement
+
 temp2.parentElement.remove()
+
 temp2.parentElement.parentElement
+
 de.appendChild(mi.parentElement)
+
 temp2.remove()
+
 de.previousElementSibling.previousElementSibling
+
 de.previousElementSibling.previousElementSibling.children[0].appendChild(temp2)
