801727b7b5c082e2b2610a42cdc34e044177419b
github <pragya.sharma2_cs19@gla.ac.in>
2021-05-11 16:31:53 +0530

    commit

--- JS_Concurrency/Promises/promise_all/script.js
+++ JS_Concurrency/Promises/promise_all/script.js
@@ -13,3 +13,18 @@ function download(url){
         }
     })
 }
+
+function resize(filename){
+    return new Promise(function(resolve, reject){
+        if(!filename.endsWith(".png")){
+            //reject()
+            reject(new Error("file is not png"))
+        } else{
+            console.log("start resizing the file : "+ filename)
+            setTimeout(function (){ //fake 0 sec to resize
+                let resizedFile = filename.split(".")[0]+"-resize.png";
+                resolve(resizedFile)
+            }) //default time is 0 .
+        }
+    })
+}
