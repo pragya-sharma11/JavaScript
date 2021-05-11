09932208a6c041a7ea8af647001db26d3c29e22d
github <pragya.sharma2_cs19@gla.ac.in>
2021-05-11 16:20:04 +0530

    commit

--- JS_Concurrency/Promises/practical_promise/script.js
+++ JS_Concurrency/Promises/practical_promise/script.js
@@ -5,7 +5,7 @@ function download(url){
             reject(new Error("url does not start with http")) 
         }
         else{
-            setTimeout(function(){
+            setTimeout(function(){ //fake 3 sec download time
                 let filename=url.split("/").pop()
                 resolve(filename)
             }, 3000)
@@ -19,12 +19,26 @@ function resize(filename){
             //reject()
             reject(new Error("file is not png"))
         } else{
-            setTimeout(function (){
+            setTimeout(function (){ //fake 0 sec to resize
                 let resizedFile = filename.split(".")[0]+"-resize.png";
                 resolve(resizedFile)
-            })
+            }) //default time is 0 .
         }
     })
+}
+
+function upload(resizefile){
+    return new Promise((resolve, reject)=>{
+    if(!resizefile.split['-']==-resize.png){
+        reject(new Error("File is not resized"))
+    }
+    else{
+        setTimeout(()=>{
+            let uploadUrl="http://upload.file/"+resizefile;
+            resolve(uploadUrl);
+        }, 3000)
+    }
+})
 }
 /*
 download('ftp://cb.lk/logo.png')
@@ -40,11 +54,13 @@ download('ftp://cb.lk/logo.png')
 */
 //can also do like
 
-download("http://cb.lk/logo.jpg")
+download("http://cb.lk/logo.png")
     .then(resize)
-    .then((resizedFile)=>{
-        console.log("resize file is at : "+resizedFile)
-    }).catch((err)=>{
+    .then(upload)
+    .then((uploadfile)=>{
+        console.log("file has been uploaded!!!") //print after 6 sec
+    })
+    .catch((err)=>{
         console.error(err)
     })
     /**
