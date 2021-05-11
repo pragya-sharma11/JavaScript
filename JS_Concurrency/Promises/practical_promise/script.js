aed0335e8e82606782042116d27e492a908e4f98
github <pragya.sharma2_cs19@gla.ac.in>
2021-05-11 15:29:57 +0530

    commit

--- JS_Concurrency/Promises/practical_promise/script.js
+++ JS_Concurrency/Promises/practical_promise/script.js
@@ -1,7 +1,26 @@
 function download(url){
     return new Promise(function(resolve, reject){
         if(!url.startsWith("http")){
-            
+           reject(new Error("url does not exist")) 
+        }
+        else{
+            setTimeout(function(){
+                let filename=url.split("/").pop()
+                resolve(filename)
+            }, 3000)
+        }
+    })
+}
+
+function resize(filename){
+    return new Promise(function(resolve, reject){
+        if(!filename.endsWith(".png")){
+            reject(new Error("Filename does not exist"))
+        } else{
+            setTimeout(function (){
+                let resizedFile = fileName+"-resize"+".png";
+                resolve(resizedFile)
+            })
         }
     })
 }
